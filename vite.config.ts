b45376365b7c4a93e3e3cefd18b3407984a004ab
nixhchay <nixhchay@gmail.com>
2025-09-21 14:56:48 +0530

    Update base path for new repository SIH-

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/SIH/", // GitHub Pages base path
+  base: "/SIH-/", // GitHub Pages base path
   server: {
     host: "::",
     port: 8080,
